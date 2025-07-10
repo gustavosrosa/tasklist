3a4b0fd51ede95a97fe686c2205dcece1c0eb719
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-10 12:06:06 -0300

    TSKL: FEAT: Gerando parte de conclusao de tarefas

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -31,6 +31,20 @@ class TaskController {
 
         return res.json(tasks);
     }
+
+    async update(req, res) {
+        const { task_id } = req.params;
+
+        const task = await Task.findByPk(task_id);
+
+        if (!task) {
+            return res.status(400).json({ error: 'Tarefa não existe' });
+        }
+
+        await task.update(req.body);
+
+        return res.json(task);
+    }
 }
 
 export default new TaskController();
