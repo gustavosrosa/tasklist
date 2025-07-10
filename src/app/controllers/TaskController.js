4b41b177773505da858521167ae46f6bdf6cb68f
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-10 11:48:03 -0300

    TSKL: FEAT: Gerando metodo criacao tarefas

---
+++ src/app/controllers/TaskController.js
@@ -0,0 +1,25 @@
+import Task from '../models/Task';
+import * as Yup from 'yup';
+
+class TaskController {
+    async store(req, res) {
+        const { task } = req.body;
+
+        const schema = Yup.object().shape({
+            task: Yup.string().required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.json({ error: 'Falha ao cadastrar' });
+        }
+
+        const tasks = await Task.create({
+            user_id: req.userId,
+            task,
+        });
+
+        return res.json(tasks);
+    }
+}
+
+export default new TaskController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/tasks', TaskController.store);
 
 export default routes;
