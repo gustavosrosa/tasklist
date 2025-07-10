7bf1a9ca30bd67edeed7e64aa184bfa056eed24a
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-10 11:07:49 -0300

    TSKL: FEAT: Validando campos

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
 import User from '../models/User';
+import * as Yup from 'yup';
+
+const MIN_LENGTH_PASSWORD = 6;
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(MIN_LENGTH_PASSWORD),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação!' });
+        }
+
         const userExists = await User.findOne({
             where: { email: req.body.email },
         });
@@ -21,6 +34,28 @@ class UserController {
         const { email, oldPassword } = req.body;
 
         const user = await User.findByPk(req.userId);
+
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(MIN_LENGTH_PASSWORD),
+            password: Yup.string()
+                .min(MIN_LENGTH_PASSWORD)
+                .when('oldPassword', (oldPassword, field) => {
+                    oldPassword ? field.required() : field;
+                }),
+            confirmPassword: Yup.string()
+                .min(MIN_LENGTH_PASSWORD)
+                .when('password', (password, field) => {
+                    password
+                        ? field.required().oneOf([Yup.ref('password')])
+                        : field;
+                }),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação!' });
+        }
 
         if (email !== user.email) {
             const userExists = await User.findOne({
